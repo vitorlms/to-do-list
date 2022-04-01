ddb0c3d0d7023440884ac53008185101913311cf
vlms <vlms@cin.ufpe.br>
2022-04-01 14:41:08 -0300

    Added connection credentials to TypeORM

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '172.17.0.3',
+      port: 3306,
+      username: 'vitor',
+      password: 'docker',
+      database: 'mysqldb',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
