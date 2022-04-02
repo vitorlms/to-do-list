17ad778290f3437f10d67f54b8ede5d79fc38d70
vlms <vlms@cin.ufpe.br>
2022-04-01 21:10:26 -0300

    added editar tarefa endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CriarTarefaDto } from './dto/criar-tarefa.dto';
+import { EditarTarefaDto } from './dto/editar-tarefa.dto';
 import { TarefaService } from './tarefa.service';
 
 @Controller()
@@ -18,5 +19,10 @@ export class AppController {
   @Post('tarefa')
   async cadastrarTarefa(criarTarefaDto: CriarTarefaDto) {
     await this.tarefaService.cadastrarTarefa(criarTarefaDto);
+  }
+
+  @Patch('tarefa')
+  async editarTarefa(@Body() editarTarefaDto: EditarTarefaDto) {
+    await this.tarefaService.editarTarefa(editarTarefaDto);
   }
 }
