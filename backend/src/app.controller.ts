1596c2000820c85ccf18d3dfc6f29b11b4ce5029
vlms <vlms@cin.ufpe.br>
2022-04-01 21:07:35 -0300

    adicionado endpoint para cadastro de tarefa

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CriarTarefaDto } from './dto/criar-tarefa.dto';
+import { TarefaService } from './tarefa.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly tarefaService: TarefaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('tarefa')
+  async cadastrarTarefa(criarTarefaDto: CriarTarefaDto) {
+    await this.tarefaService.cadastrarTarefa(criarTarefaDto);
   }
 }
